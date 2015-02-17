19f699b750a8a47d077cf58fa81eda9ee56f61b1
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2015-02-17 19:00:10 +0900

    Change command `create` -> `append`

--- karabiner/init.sh
+++ karabiner/init.sh
@@ -4,7 +4,7 @@ cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
 ./default.sh
 
-$cli create 106japanese
+$cli append 106japanese
 /bin/echo -n .
 /bin/echo "Created 106japnase settings"
 
