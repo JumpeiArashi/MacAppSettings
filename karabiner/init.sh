0157b62023d70830feb7e771eb67b3c25bb3b8b4
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2015-02-17 18:56:56 +0900

    Add initialize script

---
+++ karabiner/init.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+./default.sh
+
+$cli create 106japanese
+/bin/echo -n .
+/bin/echo "Created 106japnase settings"
+
+$cli select 1
+./106japanese.sh
