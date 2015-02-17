3cf34f6c00656ea602b8f905b6bd8453dcf2500d
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2015-02-17 19:14:52 +0900

    If non-exist 106japanese, create japanese template

--- karabiner/init.sh
+++ karabiner/init.sh
@@ -4,9 +4,13 @@ cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
 ./default.sh
 
-$cli append 106japanese
-/bin/echo -n .
-/bin/echo "Created 106japnase settings"
+if ! $cli list | grep 106japanese; then
+  $cli append 106japanese
+  /bin/echo -n .
+  /bin/echo "Appended 106japnase settings"
+fi
 
 $cli select 1
 ./106japanese.sh
+
+$cli select 0
